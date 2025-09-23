a46c0a5ae3efbaf9b947f702b322d98fe4b81909
Payal Malaghe <malaghepayal@gmail.com>
2025-09-23 17:42:26 +0530

    Find Min and Max value from given array

---
+++ MinMaxOfArray/Program.cs
@@ -0,0 +1,28 @@
+﻿// Input : {2,6,50,200,500,450,100}
+//find minimum value from array : 2
+//find maximum value from array : 500
+
+//declare array
+
+int[] Input = { 2, 6, 50, 200, 500, 450, 100 };
+Console.WriteLine("Original Array  Is: " + string.Join(' ', Input));
+int L = Input.Length;
+int min = Input[0];
+int max = Input[0];
+
+for (int i = 0; i < L; i++) 
+{
+    if (Input[i] < min) 
+    
+    {
+        min = Input[i];
+    }
+
+    if (Input[i]>max)
+    {
+        max= Input[i];
+    }
+
+}
+Console.WriteLine("Mimimum value from array Is: "+ string.Join(' ',min));
+Console.WriteLine("Mimimum value from array Is: " + string.Join(' ', max));
