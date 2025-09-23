04b816ca882c43748038da3cbdb17c5a190090d9
Payal Malaghe <malaghepayal@gmail.com>
2025-09-23 17:24:26 +0530

    Reverse the string

---
+++ ReverseString/Program.cs
@@ -0,0 +1,21 @@
+﻿// Declare a string
+
+string Input = "Hello World";
+
+
+//convert this string to character arrray.
+
+char[] InputChar = Input.ToCharArray();
+Console.WriteLine("Original String Is:" + string.Join(' ', InputChar));
+//calculate length of an array
+
+int L = InputChar.Length;
+
+for (int i =0; i < L / 2; i++) 
+{
+    char temp = InputChar[i];
+    InputChar[i] = InputChar[L - i - 1];
+    InputChar[L - i - 1] = temp;
+
+}
+Console.WriteLine("Reversed String Is:"+ string.Join(' ',InputChar));
