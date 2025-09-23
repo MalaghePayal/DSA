95ac5ff17b6ff65439e099c7a6e41f1dc033e37e
Payal Malaghe <malaghepayal@gmail.com>
2025-09-23 17:18:06 +0530

    Add project files.

---
+++ ReverseArray/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// declare intteger array
+
+int[] Numbers = {1,2,3,4,5,6,7,8 };
+Console.WriteLine("Original Array is:" + string.Join(' ', Numbers));
+//calculate length of an array
+
+int L = Numbers.Length;
+
+for (int i = 0; i < L / 2; i++)
+{ 
+
+    int temp = Numbers[i];
+    Numbers[i] = Numbers[L - i - 1];
+    Numbers[L - i - 1] = temp;
+
+}
+Console.WriteLine("Reverse Array is:"+string.Join(' ',Numbers));
